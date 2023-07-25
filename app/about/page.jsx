364d5eff5a4bd02e47af4d3611956efa837f2293
Saral Bajracharya <rmanbajra@gmail.com>
2023-07-25 19:38:12 +0545

    next-js-13

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+     <h1>About Us.</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae consequuntur esse magnam iure ad quis natus repudiandae quae libero?
+         Eum ratione debitis reprehenderit culpa laudantium quod at reiciendis rem nemo.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur assumenda blanditiis, doloremque quibusdam pariatur nulla similique iste maiores voluptate quaerat vel 
+        quia exercitationem explicabo! Non modi quisquam vitae in voluptatem?</p>
+    </div>
+  )
+}
+
+export default TeamPage
